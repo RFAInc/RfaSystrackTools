fa85203223bacc8b912c902dd7242babbac10cb1
Tony Pagliaro <tony@pagliaro.co>
2020-08-28 23:13:36 -0400

    more erroraction

--- RfaSystrackTools.psm1
+++ RfaSystrackTools.psm1
@@ -151,22 +151,22 @@ function Uninstall-Systrack {
     
     end {
         & msiexec.exe /qn /x "{5A10C299-1D99-4478-94F9-64C9DE93751D}" REBOOT=R 
-        sleep 5
+        sleep 3
 
         if (!($KeepPrerequisiteRedist)) {
             & "C:\ProgramData\Package Cache\{282975d8-55fe-4991-bbbb-06a72581ce58}\VC_redist.x64.exe"  /uninstall /quiet /norestart
-            sleep 3
+            sleep 2
             & "C:\ProgramData\Package Cache\{e31cb1a4-76b5-46a5-a084-3fa419e82201}\VC_redist.x86.exe"  /uninstall /quiet /norestart
-            sleep 5
+            sleep 3
         }
 
         gps LsiSupervisor -ea 0 | stop-process -force -ea 0
         sleep 1
 
-        del -force -recurse "C:\Windows\LtSvc\packages\SysTrack Cloud Agent\"
+        del -force -recurse "C:\Windows\LtSvc\packages\SysTrack Cloud Agent\" -ea 0
         sleep 1
 
-        del -force -recurse "C:\Program Files (x86)\SysTrack\"
+        del -force -recurse "C:\Program Files (x86)\SysTrack\" -ea 0
         sleep 2
         
         del -force -recurse "HKLM:\SOFTWARE\WOW6432Node\Lakeside Software\" -ea 0
